c796464419dc008a4416200d7bc0098a451ca4dc
abimaelhernandez <hernandezabimael364@gmail.com>
2018-08-31 16:00:56 -0500

    start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,3 @@
 ///react will render properly when component App created
-//remember to use a class component  to pass props
-//hit Api ("http://174.138.36.217/people/")
+//remember to use a class . js to pass props
+//hit Api ("http://174.138.36.217/people/")a
